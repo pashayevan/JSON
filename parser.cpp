5a368f44e09a9832cc30f71febc5f4627205482d
pashayevan <nairapash@gmail.com>
2025-03-01 14:44:13 +0400

    01.03

--- parser.cpp
+++ parser.cpp
@@ -8,24 +8,40 @@ void JSONParser::parse() {
                token=tokenizer.getToken();
                std::cout<< token.toString()<< "\n";
                switch (token.type) {
-               case TOKEN::CURLY_OPEN;
+                    case TOKEN::CURLY_OPEN;
                     std::shared_ptr<<JSON::JSONNode> parsedObject = parseObject();
                     parsedObject()->printNode(0);
                     if (!root) {
                          root = parsedObject;
                     }
                     break;
-               case TOKEN::ARRAY_OPEN;
+                    case TOKEN::ARRAY_OPEN;
                     std::shared_ptr<<JSON::JSONNode> parsedList = parseList();
                     if (!root) {
                          root = parsedList;
-                     }
+                    }
                     break;
-               case TOKEN::STRING;
+                    case TOKEN::STRING;
                     tokenizer.rollBackToken();
                     std::shared_ptr<<JSON::JSONNode> parsedString = parseString();
                     if (!root) {
                          root = parsedString;
+                    }
+                    break;
+                    case TOKEN::NUMBER;
+                    tokenizer.rollBackToken();
+                    std::shared_ptr<<JSON::JSONNode> parsedNumber = parseNumber();
+                    if (!root) {
+                         root = parsedNumber;
+                    }
+                    break;
+                    case TOKEN::BOOLEAN;
+                    tokenizer.rollBackToken();
+                    std::shared_ptr<<JSON::JSONNode> parsedBoolean = parseBoolean();
+                    break;
+               }
+          } catch (std::logic_error e) {
+               break;
           }
      }
 }
